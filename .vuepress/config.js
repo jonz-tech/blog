06fe4c6060f0232fc3c8e55aedf04cca914efa66
zhuangchubin <zhuangchubin@yy.com>
2020-04-15 20:26:13 +0800

    add

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: '文档',
+  description: 'Document library',
+  head: [
+    // ['link', { rel: 'icon', href: `/favicon.ico` }],
+  ],
+   themeConfig: {
+    nav: [
+      { text: '主页', link: '/home/' },
+    ]
+  }
+}
