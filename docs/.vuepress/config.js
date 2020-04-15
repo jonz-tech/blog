f131b9bfd7898667c146a208fd40a70cd1abdffc
zhuangchubin <zhuangchubin@yy.com>
2020-04-15 19:35:34 +0800

    vueData

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	locales: {
+        '/': {
+            lang: 'zh-CN',
+            title: '千里之行，始于足下',
+            description: ''
+        }
+    },
+   themeConfig: {
+        repo: 'https://jonz-tech.github.io/blog/',
+        repoLabel: 'My GitHub',
+        nav: [
+            { text: 'home', link: '/home/' },
+            { text: 'fullter', link: '/Flutter/' }
+        ]
+    }
+}
